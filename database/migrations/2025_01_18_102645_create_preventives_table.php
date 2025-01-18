71014751c754f80c7fe7d96d6d2c6a82c04f2e5c
gaetanovenuto <gaetanovenuto99@live.it>
2025-01-18 12:12:21 +0100

    Created base table structure

--- database/migrations/2025_01_18_102645_create_preventives_table.php
+++ database/migrations/2025_01_18_102645_create_preventives_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('preventives', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('user')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('car_id')->constrained('car')->onUpdate('cascade')->onDelete('cascade');
+            $table->decimal('total_price', 10, 2);
+            $table->timestamp('creation_date')->default(now());
             $table->timestamps();
         });
     }
